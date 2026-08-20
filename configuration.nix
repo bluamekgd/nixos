5c65c97dd564de5067bf9a552da5141d41092517
BluamekGD <bartosz.nicely@gmail.com>
2026-08-21 01:36:38 +0200

    umm maybe this

--- configuration.nix
+++ configuration.nix
@@ -146,12 +146,13 @@
   };
 
   # Ly DM
+  environment.etc."ly.dur".source = ./resources/ly.dur;
   services.displayManager.ly = {
     enable = true;
     settings = {
       # Animation
       animation = "dur_file";
-      dur_file_path = "./resources/ly.dur";
+      dur_file_path = "/etc/ly.dur";
       dur_x_offset = 55;
       dur_y_offset = -5;
       animation_timeout_sec = 0;
